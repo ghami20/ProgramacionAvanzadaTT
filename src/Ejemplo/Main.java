a78bb0928c5624929509c21b7b21a45f5931e5b1
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-09-20 17:39:15 -0300

    Hice otro cambio

--- src/Ejemplo/Main.java
+++ src/Ejemplo/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hola primera prueba");
+			
+			System.out.println("Cree un cambio");
 	}
 
 }
